5db8a4a078972c4fc5d0a540579c07215c4d766b
Grant Sanderson <grant@3blue1brown.com>
2021-01-03 14:39:31 -0800

    Added file for deprecated code relevant to old videos

---
+++ custom/deprecated.py
@@ -0,0 +1,22 @@
+# This file contains functions and classes which have been used in old
+# videos, but which have since been supplanted in manim.  For example,
+# there were previously various specifically named classes for version
+# of fading which are now covered by arguments passed into FadeIn and
+# FadeOut
+
+from manimlib.imports import *
+
+
+class FadeInFromDown(FadeIn):
+    def __init__(self, mobject, **kwargs):
+        super().__init__(mobject, UP, **kwargs)
+
+
+class FadeOutAndShiftDown(FadeOut):
+    def __init__(self, mobject, **kwargs):
+        super().__init__(mobject, DOWN, **kwargs)
+
+
+class FadeInFromLarge(FadeIn):
+    def __init__(self, mobject, scale_factor=2, **kwargs):
+        super().__init__(mobject, scale=(1 / scale_factor), **kwargs)
--- manim_imports_ext.py
+++ manim_imports_ext.py
@@ -4,6 +4,7 @@ from custom.banner import *
 from custom.characters.pi_creature import *
 from custom.characters.pi_creature_animations import *
 from custom.characters.pi_creature_scene import *
+from custom.deprecated import *
 from custom.end_screen import *
 from custom.filler import *
 from custom.logo import *
