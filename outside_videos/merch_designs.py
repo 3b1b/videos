ddc4be4df5570eb0fecbc7243864895dd65594e9
Grant Sanderson <grant@3blue1brown.com>
2021-03-18 17:27:35 -0700

    Change to sok

--- outside_videos/merch_designs.py
+++ outside_videos/merch_designs.py
@@ -1,4 +1,5 @@
 from manim_imports_ext import *
+from manimlib.once_useful_constructs.fractals import *
 
 
 # Fractal posters
@@ -50,39 +51,33 @@ class ShowSierpinski(ShowHilbertCurve):
 
 # Socks
 class SquareWave(Scene):
-    CONFIG = {
-        "L": FRAME_WIDTH / 4,
-    }
-
     def construct(self):
-        kwargs = {
-            "x_min": -2 * self.L,
-            "x_max": 2 * self.L,
-        }
+        L = FRAME_WIDTH / 4
         waves = VGroup(*[
             FunctionGraph(
-                lambda x: self.approx_square_wave(x, n_terms),
-                **kwargs
+                lambda x: self.approx_square_wave(x, L, n_terms),
+                x_range=[-2 * L, 2 * L, L / 50],
             )
             for n_terms in range(1, 5)
         ])
         waves.set_color_by_gradient(BLUE, YELLOW, RED)
         stroke_widths = np.linspace(3, 1, len(waves))
         for wave, stroke_width in zip(waves, stroke_widths):
-            wave.set_stroke(width=stroke_width)
-        waves.to_edge(UP)
-        self.add(waves)
+            # wave.set_stroke(width=stroke_width)
+            wave.set_stroke(width=3)
+        # waves.to_edge(UP)
 
         tex_mob = Tex("""
             {4 \\over \\pi}
-            \\sum_{n=1,3,5,\\dots} {1 \\over n}\\sin(n\\pi x)
+            \\sum_{n=0}^\\infty {\\sin\\big((2n + 1)\\pi x\\big) \\over (2n + 1)}
         """)
         tex_mob.scale(1.5)
-        tex_mob.next_to(waves, DOWN, MED_LARGE_BUFF)
-        self.add(tex_mob)
+        # tex_mob.next_to(waves, DOWN, MED_LARGE_BUFF)
 
-    def approx_square_wave(self, x, n_terms=3):
-        L = self.L
+        self.add(waves)
+        # self.add(tex_mob)
+
+    def approx_square_wave(self, x, L, n_terms=3):
         return 1.0 * sum([
             (1.0 / n) * np.sin(n * PI * x / L)
             for n in range(1, 2 * n_terms + 1, 2)
