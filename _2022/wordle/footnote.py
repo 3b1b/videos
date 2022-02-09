57aaeb62eeb1081a2819510f91da493a7bdc90a3
Grant Sanderson <grant@3blue1brown.com>
2022-02-09 07:43:11 -0800

    Stubs for a footnote

---
+++ _2022/wordle/footnote.py
@@ -0,0 +1,12 @@
+from manim_imports_ext import *
+from _2022.wordle.scenes import *
+
+
+class HowWordleColoringWorks(Scene):
+    def construct(self):
+        pass
+
+
+class HowBruteForcingWorks(Scene):
+    def construct(self):
+        pass
