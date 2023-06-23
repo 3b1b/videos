9b3aa71c89e6cfae9407aef6086e135f5c1f9c27
Grant Sanderson <grant@3blue1brown.com>
2023-06-23 10:57:35 -0700

    Change end screen message

--- custom/end_screen.py
+++ custom/end_screen.py
@@ -35,7 +35,7 @@ class PatreonEndScreen(Scene):
     randomize_order = False
     capitalize = True
     name_y_spacing = 0.6
-    thanks_words = "Funded by viewers, visit 3b1b.co/support to learn more"
+    thanks_words = "An especially warm thanks to these patrons"
     scroll_time = 20
 
     def construct(self):
